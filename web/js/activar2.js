9d6aff9b032b5fc4c514781f8e6a273b79c829e6
J <madc4psul3z@gmail.com>
2026-02-03 19:20:19 +0100

    arreglado activar

--- web/js/activar2.js
+++ web/js/activar2.js
@@ -10,14 +10,17 @@ window.onload = () => {
 }
 
 async function func_sendcheck(event) {
-    let value = event.target.closest('input').value;
-    let value2 = event.target.value;
-    console.log(value);
+    let value = event.target.previousElementSibling.value;
+    let value2 = event.target.checked;
+
+    value2 = value2 == true ? 1 : 0;
+
+    console.log(value2);
     let petition = new Request(`index.php?ctl=activarUser&id=${value}&act=${value2}`,
         {
             method: "GET",
         });
-
+console.log(petition);
     try {
         let info = await fetch(petition);
         if (info.ok) {
