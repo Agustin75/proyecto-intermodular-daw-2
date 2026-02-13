a002f0ffb46085c23f38f93effedd13e752ae68e
Agustin <agusconti33@hotmail.com>
2026-02-14 00:47:18 +0100

    Quitado el head del archivo inicio.php

--- app/templates/inicio.php
+++ app/templates/inicio.php
@@ -1,13 +1,5 @@
 <?php ob_start() ?>
-<head> 
-    <link rel=stylesheet href="../web/css/estilo.css" type="text/css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
-</head>
 <h1 class="text-center"><?php echo $params['page_title'] ?></h1>
-
-
 
 <?php foreach ($params['paragraphs'] as $paragraph): ?>
     <p class="text-center"><?= $paragraph ?></p>
